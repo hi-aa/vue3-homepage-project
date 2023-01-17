df24b1c20d51f19726d8692a1a258dbaf0750731
hi-aa <ost.jj1201@gmail.com>
2023-01-17 21:01:30 +0900

    tab 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,74 +5,31 @@
 			<button
 				v-for="(tab, index) in TAB_INFO"
 				:key="index"
+				:class="{ active: selectedTab == index }"
 				@click="selectedTab = index"
 			>
-				{{ tab.title }}
+				{{ tab.buttonText }}
 			</button>
-			<!-- <button @click="selectedTab = 0">Main Tab 1</button>
-		<button @click="selectedTab = 1">Main Tab 2</button>
-		<button @click="selectedTab = 2">Main Tab 3</button> -->
 		</div>
-
 		<div class="desc">
-			<h2>
-				{{ TAB_INFO[selectedTab].subTitle }}
-			</h2>
-			<p>
-				What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-				and typesetting industry. Lorem Ipsum has been the industry's standard
-				dummy text ever since the 1500s, when an unknown printer took a galley
-				of type and scrambled it to make a type specimen book. It has survived
-				not only five centuries, but also the leap into electronic typesetting,
-				remaining essentially unchanged. It was popularised in the 1960s with
-				the release of Letraset sheets containing Lorem Ipsum passages, and more
-				recently with desktop publishing software like Aldus PageMaker including
-				versions of Lorem Ipsum.
-			</p>
-			<p>
-				Why do we use it? It is a long established fact that a reader will be
-				distracted by the readable content of a page when looking at its layout.
-				The point of using Lorem Ipsum is that it has a more-or-less normal
-				distribution of letters, as opposed to using 'Content here, content
-				here', making it look like readable English. Many desktop publishing
-				packages and web page editors now use Lorem Ipsum as their default model
-				text, and a search for 'lorem ipsum' will uncover many web sites still
-				in their infancy. Various versions have evolved over the years,
-				sometimes by accident, sometimes on purpose (injected humour and the
-				like).
-			</p>
-			<p>
-				Where does it come from? Contrary to popular belief, Lorem Ipsum is not
-				simply random text. It has roots in a piece of classical Latin
-				literature from 45 BC, making it over 2000 years old. Richard
-				McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-				looked up one of the more obscure Latin words, consectetur, from a Lorem
-				Ipsum passage, and going through the cites of the word in classical
-				literature, discovered the undoubtable source. Lorem Ipsum comes from
-				sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
-				Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
-				treatise on the theory of ethics, very popular during the Renaissance.
-				The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
-				from a line in section 1.10.32.
-				<br />
-				The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-				below for those interested. Sections 1.10.32 and 1.10.33 from "de
-				Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact
-				original form, accompanied by English versions from the 1914 translation
-				by H. Rackham.
-			</p>
+			<KeepAlive>
+				<component :is="TAB_INFO[selectedTab].view"></component>
+			</KeepAlive>
 		</div>
 	</div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import ContentsView1 from './tab1/ContentsView1.vue';
+import ContentsView2 from './tab1/ContentsView2.vue';
+import ContentsView3 from './tab1/ContentsView3.vue';
 
 const selectedTab = ref(0);
 const TAB_INFO = [
-	{ title: 'First Temp', subTitle: 'Sub Title First', alt: 'blabla' },
-	{ title: '222 Temp', subTitle: 'a', alt: 'slide show' },
-	{ title: 'Others', subTitle: 'b', alt: 'what else?' },
+	{ view: ContentsView1, buttonText: 'First Temp' },
+	{ view: ContentsView2, buttonText: '2 Temp' },
+	{ view: ContentsView3, buttonText: '3 Temp' },
 ];
 </script>
 
@@ -109,7 +66,8 @@ button {
 }
 
 /* Add a background color on hover */
-.btn-group button:hover {
+.btn-group button:hover,
+.btn-group button.active {
 	background-color: #b2bebf;
 }
 .desc {
---
+++ src/views/tab1/ContentsView3.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<h2>33</h2>
+		<p>333</p>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
